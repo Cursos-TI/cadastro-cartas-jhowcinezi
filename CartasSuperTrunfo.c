d398c613c66ddaad4998d4212caf2c6082266d56
Cinezi <jonathascinezi@gmail.com>
2025-11-06 17:23:45 +0000

    Nível Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,128 @@
 #include <stdio.h>
+#include <string.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+    //Declaração das Variáveis - Carta 1
+    char id_1[20];
+    char state_name_1[20];
+    char city_name_1[20];
+    int popl_1;
+    float area_size_1;
+    float pib_1;
+    int tour_att_1;
 
-  // Área para exibição dos dados da cidade
+    //Declaração das Variáveis - Carta 2
+    char id_2[20];
+    char state_name_2[20];
+    char city_name_2[20];
+    int popl_2;
+    float area_size_2;
+    float pib_2;
+    int tour_att_2;
 
-return 0;
-} 
+
+
+    //Menu
+    printf("DESAFIO SUPER TRUNFO EM C\n");
+    printf("----------------------------\n\n");
+
+
+
+    //Coleta de Informações - Carta 1
+    printf("POR FAVOR INSIRA AS INFORMAÇÕES DA CARTA 1\n\n");
+
+    printf("Digite o Código da Carta:\n");
+    scanf("%s", id_1);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+    printf("\nDigite o Nome do Estado:\n");
+    fgets(state_name_1, sizeof(state_name_1), stdin);
+    state_name_1[strcspn(state_name_1, "\n")] = '\0'; // <-- Faz o tratamento da String para procurar o "\n" (do Enter) e substituir por "\0" (Vazio)
+
+    printf("\nDigite o Nome da Cidade:\n");
+    fgets(city_name_1, sizeof(city_name_1), stdin);
+    city_name_1[strcspn(city_name_1, "\n")] = '\0'; // <-- Faz o tratamento da String para procurar o "\n" (do Enter) e substituir por "\0" (Vazio)
+
+    printf("\nDigite a População da Cidade:\n");
+    scanf("%d", &popl_1);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+    printf("\nDigite a Área em Km² da Cidade:\n");
+    scanf("%f", &area_size_1);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+    printf("\nDigite o PIB da Cidade:\n");
+    scanf("%f", &pib_1);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+    printf("\nDigite a quantidade de Pontos Turísticos da Cidade:\n");
+    scanf("%d", &tour_att_1);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+
+    //Coleta de Informações - Carta 2
+    printf("\n\nPOR FAVOR INSIRA AS INFORMAÇÕES DA CARTA 2\n\n");
+
+    printf("Digite o Código da Carta:\n");
+    scanf("%s", id_2);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+    printf("\nDigite o Nome do Estado:\n");
+    fgets(state_name_2, sizeof(state_name_2), stdin);
+    state_name_2[strcspn(state_name_2, "\n")] = '\0'; // <-- Faz o tratamento da String para procurar o "\n" (do Enter) e substituir por "\0" (Vazio)
+
+    printf("\nDigite o Nome da Cidade:\n");
+    fgets(city_name_2, sizeof(city_name_2), stdin);
+    city_name_2[strcspn(city_name_2, "\n")] = '\0'; // <-- Faz o tratamento da String para procurar o "\n" (do Enter) e substituir por "\0" (Vazio)
+
+
+    printf("\nDigite a População da Cidade:\n");
+    scanf("%d", &popl_2);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+    printf("\nDigite a Área em Km² da Cidade:\n");
+    scanf("%f", &area_size_2);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+    printf("\nDigite o PIB da Cidade:\n");
+    scanf("%f", &pib_2);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+    printf("\nDigite a quantidade de Pontos Turísticos da Cidade:\n");
+    scanf("%d", &tour_att_2);
+    while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
+
+
+
+    //Exibição das Informações - Carta 1
+    printf("\n\nCARTA 1\n");
+    printf("----------------------------\n");
+    printf("Código: %s\n", id_1);
+    printf("Estado: %s\n", state_name_1); 
+    printf("Cidade: %s\n", city_name_1);
+    printf("População da Cidade: %d\n", popl_1);
+    printf("Área da Cidade: %.2f", area_size_1); printf(" Km²\n");
+    printf("PIB da Cidade: %.2f", pib_1); printf(" Bilhões de Reais\n");
+    printf("Quantidade de Pontos Turísticos da Cidade: %d\n\n", tour_att_1);
+
+
+    //Exibição das Informações - Carta 2
+    printf("\n\nCARTA 2\n");
+    printf("----------------------------\n");
+    printf("Código: %s\n", id_2);
+    printf("Estado: %s\n", state_name_2); 
+    printf("Cidade: %s\n", city_name_2);
+    printf("População da Cidade: %d\n", popl_2);
+    printf("Área da Cidade: %.2f", area_size_2); printf(" Km²\n");
+    printf("PIB da Cidade: %.2f", pib_2); printf(" Bilhões de Reais\n");
+    printf("Quantidade de Pontos Turísticos da Cidade: %d\n\n", tour_att_2);
+
+
+
+    return 0;
+}
