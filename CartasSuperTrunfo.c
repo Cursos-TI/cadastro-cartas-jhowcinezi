3f1be2362dcd39923e09c431b17cbb12a7d686cc
Cinezi <jonathascinezi@gmail.com>
2025-11-10 15:30:09 +0000

    Nível Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main() {
 
     //Declaração das Variáveis - Carta 1
     char id_1[20];
     char state_name_1[20];
     char city_name_1[20];
-    int popl_1;
+    unsigned long int popl_1;
     float area_size_1;
     float pib_1;
     int tour_att_1;
     float popl_dens_1;
     float pib_pcap_1;
+    float spower_1;
 
 
     //Declaração das Variáveis - Carta 2
     char id_2[20];
     char state_name_2[20];
     char city_name_2[20];
-    int popl_2;
+    unsigned long int popl_2;
     float area_size_2;
     float pib_2;
     int tour_att_2;
     float popl_dens_2;
     float pib_pcap_2;
+    float spower_2;
 
 
     //Menu
@@ -102,13 +100,15 @@ int main() {
     while (getchar() != '\n'); // <-- Limpa o Buffer, pois o ScanF armazena o "Enter" e entende como "\n"
 
 
-   //Cálculo das Informações - Carta 1
+    //Cálculo das Informações - Carta 1
     popl_dens_1 = (float) popl_1 / area_size_1;
     pib_pcap_1 = (float) pib_1 / popl_1;
+    spower_1 = (float) popl_1 + area_size_1 + pib_1 + (float) tour_att_1 + popl_dens_1 + pib_pcap_1;
    
-   //Cálculo das Informações - Carta 2
+    //Cálculo das Informações - Carta 2
     popl_dens_2 = (float) popl_2 / area_size_2;
     pib_pcap_2 = (float) pib_2 / popl_2;
+    spower_2 = (float) popl_2 + area_size_2 + pib_2 + (float) tour_att_2 + popl_dens_2 + pib_pcap_2;
 
 
     //Exibição das Informações - Carta 1
@@ -122,7 +122,8 @@ int main() {
     printf("PIB da Cidade: %.2f", pib_1); printf(" Bilhões de Reais\n");
     printf("Quantidade de Pontos Turísticos da Cidade: %d\n", tour_att_1);
     printf("Densidade Populacional: %.2f", popl_dens_1); printf(" Hab/Km²\n");
-    printf("PIB per Capita: %.2f", pib_pcap_1); printf(" Reais\n\n");
+    printf("PIB per Capita: %.2f", pib_pcap_1); printf(" Reais\n");
+    printf("Super Poder: %.2f\n\n", spower_1); 
 
 
     //Exibição das Informações - Carta 2
@@ -136,9 +137,30 @@ int main() {
     printf("PIB da Cidade: %.2f", pib_2); printf(" Bilhões de Reais\n");
     printf("Quantidade de Pontos Turísticos da Cidade: %d", tour_att_2);
     printf("Densidade Populacional: %.2f", popl_dens_2); printf(" Hab/Km²\n");
-    printf("PIB per Capita: %.2f", pib_pcap_2); printf(" Reais\n\n");
+    printf("PIB per Capita: %.2f", pib_pcap_2); printf(" Reais\n");
+    printf("Super Poder: %.2f\n\n", spower_2); 
 
 
+    //Cálculo da Comparação das Cartas
+    int popl_r = popl_1 < popl_2;
+    int area_size_r = area_size_1 < area_size_2;
+    int pib_r = pib_1 < pib_2;
+    int tour_att_r  = tour_att_1 < tour_att_2;
+    int popl_dens_r = tour_att_1 > tour_att_2;
+    int pib_pcap_r = pib_pcap_1 < pib_pcap_2;
+    int spower_r = spower_1 < spower_2;
+
+
+    //Exibição da Comparação das Informações
+    printf("COMPARAÇÃO DAS CARTAS\n");
+    printf("----------------------------\n");
+    printf("População da Cidade: %d\n", popl_r);
+    printf("Área da Cidade: %d\n", area_size_r);
+    printf("PIB da Cidade: %d\n", pib_r);
+    printf("Quantidade de Pontos Turísticos da Cidade: %d\n", tour_att_r);
+    printf("Densidade Populacional: %d\n" , popl_dens_r);
+    printf("PIB per Capita: %d\n", pib_pcap_r);
+    printf("Super Poder: %d\n\n", spower_r); 
 
     return 0;
 }
